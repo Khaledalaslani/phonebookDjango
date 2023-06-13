a8ff77f4966b63ad9924d9836c8c1a71cf4d1c90
khaledalaslani <khaledalaslani1@gmail.com>
2023-06-13 14:23:15 +0300

    add models

--- phoneBookApp/models.py
+++ phoneBookApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Number(models.Model):
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE, related_name='numbers')
+    number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.number 
